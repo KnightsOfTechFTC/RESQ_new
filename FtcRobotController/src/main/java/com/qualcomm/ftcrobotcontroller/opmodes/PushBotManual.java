db3daaee7eaa919e572ca0c4f2f79bb74dd179f4
Lego5 <ftcknightsoftech@gmail.com>
2016-02-23 18:20:43 -0600

    Made tape_extend run slower(to avoid torquing) in Manual

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManual.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManual.java
@@ -174,8 +174,8 @@ public class PushBotManual extends PushBotTelemetry
         // The setPosition methods write the motor power values to the Servo
         // class, but the positions aren't applied until this method ends.
         //
-        if (gamepad1.dpad_down){m_tape_extend_pover(1);}
-        else if (gamepad1.dpad_up){m_tape_extend_pover(-1);}
+        if (gamepad1.dpad_down){m_tape_extend_pover(.8);}
+        else if (gamepad1.dpad_up){m_tape_extend_pover(-.8);}
         else {m_tape_extend_pover(0);}
     //    if (gamepad1.dpad_right){m_tape_angle_position(a_tape_angle_position()+.01);}
     //    if (gamepad1.dpad_left){m_tape_angle_position(a_tape_angle_position() - .01);}
